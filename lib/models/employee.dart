bae4294f57800371b074ad5fa339a85890477c71
marcellinus Yovian Indrastata <marcellinusyovian@gmail.com>
2023-12-10 23:03:09 +0700

    Add Employee Model

---
+++ lib/models/employee.dart
@@ -0,0 +1,13 @@
+class Employee {
+  String? uid;
+  String? employeeName;
+  String? position;
+  String? contactNumber;
+
+  Employee({
+    this.uid,
+    this.employeeName,
+    this.position,
+    this.contactNumber,
+  });
+}
